5a6a475e017697fceb7f1f9f5025bc083025cad7
BommakantiAlekhya <71487433+BommakantiAlekhya@users.noreply.github.com>
2021-07-27 13:34:57 +0530

    ..

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,43 @@
 
 
 
-def fun_nth_happy_prime(n):
-	return 0
+def isprime(x):
+    count=0
+    if x>1:
+        for i in range(1,x+1):
+            if(x%i)==0:
+                count = count+1
+        if count == 2:
+            return True
+        else:
+            return False
+def powerfulnumber(x):
+    if(x==1):
+        return True
+    
+    else:
+        
+        count=0
+        count2=0
+        for i in range(2,int(x/2)+1):
+            if(x%i==0 and isprime(i)):
+                count+=1
+                if(x%(i*i)==0):
+                    count2+=1
+        print(count,count2)
+        if(count==0 or count2==0):
+            return False
+        elif(count==count2):
+            return True
+
+    return False
+
+def nthpowerfulnumber(n):
+
+    found = 0
+    guess = 0
+    while (found <= abs(n)):
+        guess += 1
+        if(powerfulnumber(guess)):
+            found += 1
+    return guess
